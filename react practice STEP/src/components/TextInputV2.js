e3ce8b53e08e95d9fb3b4bcc2e91808b37b4ccd9
shuvethram137 <53695045+VMuthulakshmi@users.noreply.github.com>
2023-11-19 12:31:51 +0530

    added

--- react practice STEP/src/components/TextInputV2.js
+++
@@ -1,8 +0,0 @@
-import "../styles/textInputStyles.css";
-
-const TextInputV2 = (props) => {
-    return <input name={props.placeholder} type={props.type} className={props.className} placeholder={props.placeholder} value={props.value} onChange={(event) => props.onChange(event.target.value)}>
-    </input>
-}
-
-export default TextInputV2;

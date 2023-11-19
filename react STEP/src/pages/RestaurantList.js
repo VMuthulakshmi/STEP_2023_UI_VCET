5550838098dc77e9a245b4104bb8131d176dc4f4
shuvethram137 <53695045+VMuthulakshmi@users.noreply.github.com>
2023-11-19 12:37:56 +0530

    have updated the changes

--- react STEP/src/pages/RestaurantList.js
+++ react STEP/src/pages/RestaurantList.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import "../styles/restaurantListPageStyles.css";
+import HeaderBar from "../components/HeaderBar";
+import { RestaurantService } from "../services/RestaurantService";
+import RestaurantCard from "../components/RestaurantCard";
+
 const RestaurantList = () => {
-    return <h1>restaurant list page</h1>
+
+//TODO: initalize retsturant state.
+
+  /**
+ * Fetch the list of restaurants from RestaurantService and updates the component state.
+ * Sets the state to display loading indicator during data fetch from service.
+ */
+  async function fetchRestaurantList() {
+    //setRestaurantState({...restaurantState, isLoading:true})
+    let restaurantsList = await RestaurantService.getRestaurantsList();
+   //TODO: set states according to the result.
+  }
+
+  useEffect(() => {
+    fetchRestaurantList();
+  }, [])
+
+
+  return (
+    <div className="restaurant-list-container">
+      <HeaderBar />
+      <div className="restaurant-list-wrapper child">
+        {/* //TODO: Iterate the result */}
+      </div>
+    </div>
+  );
 }
 
 export default RestaurantList;

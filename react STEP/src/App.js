69cc4177f6bfc288196bdb702365838ff13fd342
VMuthulakshmi <vmuthulakshmi1803@gmail.com>
2023-11-22 18:11:47 +0530

    Updated with third page

--- react STEP/src/App.js
+++ react STEP/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import LoginPage from "./pages/LoginPage";
 import {  Routes, Route } from "react-router-dom";
-import "./App.css";
+
+import LoginPage from "./pages/LoginPage";
+import OrdersPage from "./pages/OrdersPage";
 import RestaurantList from "./pages/RestaurantList";
+import "./App.css";
+
 function App() {
   return (
     <div>
       <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/restaurantList" element={<RestaurantList />} />
+          <Route path={"/orders/:id"} element={<OrdersPage />}
+      ></Route>
       </Routes>
     </div>
 
--- react STEP/src/pages/RestaurantList.js
+++ react STEP/src/pages/RestaurantList.js
@@ -6,16 +6,21 @@ import RestaurantCard from "../components/RestaurantCard";
 
 const RestaurantList = () => {
 
-//TODO: initalize retsturant state.
+  //TODO: initalize retsturant state.
+  const [restaurantState, setRestaurantState ]= useState([]);
 
   /**
  * Fetch the list of restaurants from RestaurantService and updates the component state.
  * Sets the state to display loading indicator during data fetch from service.
  */
   async function fetchRestaurantList() {
-    //setRestaurantState({...restaurantState, isLoading:true})
     let restaurantsList = await RestaurantService.getRestaurantsList();
    //TODO: set states according to the result.
+    if (restaurantsList.length > 0) {
+      setRestaurantState(restaurantsList);
+    
+    }
+    
   }
 
   useEffect(() => {
@@ -28,6 +33,17 @@ const RestaurantList = () => {
       <HeaderBar />
       <div className="restaurant-list-wrapper child">
         {/* //TODO: Iterate the result */}
+        {restaurantState.map(res => {
+          return (
+            <div>
+              <RestaurantCard
+                key={res.id}
+                restaurant={res}
+                index={res.id}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
